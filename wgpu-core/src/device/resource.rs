52d8313475f1c4ee1b2678d9f63ed4720cfb280f
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-04-17 20:50:31 +0100

    Allow configuring whether workgroup memory is zero initialised (#5508)

--- wgpu-core/src/device/resource.rs
+++ wgpu-core/src/device/resource.rs
@@ -2793,6 +2793,7 @@ impl<A: HalApi> Device<A> {
                 module: shader_module.raw(),
                 entry_point: final_entry_point_name.as_ref(),
                 constants: desc.stage.constants.as_ref(),
+                zero_initialize_workgroup_memory: desc.stage.zero_initialize_workgroup_memory,
             },
         };
 
@@ -3208,6 +3209,7 @@ impl<A: HalApi> Device<A> {
                 module: vertex_shader_module.raw(),
                 entry_point: &vertex_entry_point_name,
                 constants: stage_desc.constants.as_ref(),
+                zero_initialize_workgroup_memory: stage_desc.zero_initialize_workgroup_memory,
             }
         };
 
@@ -3268,6 +3270,9 @@ impl<A: HalApi> Device<A> {
                     module: shader_module.raw(),
                     entry_point: &fragment_entry_point_name,
                     constants: fragment_state.stage.constants.as_ref(),
+                    zero_initialize_workgroup_memory: fragment_state
+                        .stage
+                        .zero_initialize_workgroup_memory,
                 })
             }
             None => None,
