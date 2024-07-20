f8a0fa28b90207db21c766c9e090d371da019afb
Bruce Mitchener <bruce.mitchener@gmail.com>
2024-07-20 10:35:19 +0700

    Remove `allow(unknown_lints)` and `clippy::if_then_panic` (#6000)

--- wgpu-types/src/lib.rs
+++ wgpu-types/src/lib.rs
@@ -1497,7 +1497,6 @@ impl Limits {
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct DownlevelLimits {}
 
-#[allow(unknown_lints)] // derivable_impls is nightly only currently
 #[allow(clippy::derivable_impls)]
 impl Default for DownlevelLimits {
     fn default() -> Self {
