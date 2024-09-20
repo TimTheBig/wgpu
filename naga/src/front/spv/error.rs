81a8d38a5b1b53c245831cab1dd1e601d800b5e3
Erich Gubler <erichdongubler@gmail.com>
2024-09-20 03:29:05 -0400

    chore: un-regress `unused_qualifications` lint in `naga::front::spv` (#6297)
    Regressed in #5824.

--- naga/src/front/spv/error.rs
+++ naga/src/front/spv/error.rs
@@ -164,6 +164,6 @@ impl Error {
 
 impl From<atomic_upgrade::Error> for Error {
     fn from(source: atomic_upgrade::Error) -> Self {
-        crate::front::spv::Error::AtomicUpgradeError(source)
+        Error::AtomicUpgradeError(source)
     }
 }
