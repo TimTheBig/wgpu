a3d0f3da3a3de915f0a60cdce02e75a87cbaf191
ChosenName <69129796+ChosenName@users.noreply.github.com>
2024-10-08 07:00:00 -0600

    [naga] added DrawID (#6325)

--- naga/src/back/spv/writer.rs
+++ naga/src/back/spv/writer.rs
@@ -1629,6 +1629,7 @@ impl Writer {
                     Bi::InstanceIndex => BuiltIn::InstanceIndex,
                     Bi::PointSize => BuiltIn::PointSize,
                     Bi::VertexIndex => BuiltIn::VertexIndex,
+                    Bi::DrawID => BuiltIn::DrawIndex,
                     // fragment
                     Bi::FragDepth => BuiltIn::FragDepth,
                     Bi::PointCoord => BuiltIn::PointCoord,
