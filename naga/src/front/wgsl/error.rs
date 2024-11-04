ac14931c337c69971b9f3da04895e73ef46ee112
Erich Gubler <erichdongubler@gmail.com>
2024-11-04 17:02:21 -0500

    feat(wgsl-in): add unimpl. diag. for compound stmt. `@diagnostic(…)`s

--- naga/src/front/wgsl/error.rs
+++ naga/src/front/wgsl/error.rs
@@ -136,6 +136,8 @@ pub enum ExpectedToken<'a> {
     Variable,
     /// Access of a function
     Function,
+    /// The `diagnostic` identifier of the `@diagnostic(…)` attribute.
+    DiagnosticAttribute,
 }
 
 #[derive(Clone, Copy, Debug, Error, PartialEq)]
@@ -390,6 +392,9 @@ impl<'a> Error<'a> {
                     }
                     ExpectedToken::AfterIdentListComma => {
                         "next argument or end of list (';')".to_string()
+                    }
+                    ExpectedToken::DiagnosticAttribute => {
+                        "the 'diagnostic' attribute identifier".to_string()
                     }
                 };
                 ParseError {
