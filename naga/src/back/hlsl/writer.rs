9a0b811d8bb24d4405336a3096561eb803214a96
Jamie Nicol <jamie@jamiern.uk>
2024-11-12 11:05:19 +0000

    [naga] Implement quantizeToF16 (#6519)
    Implement WGSL frontend and WGSL, SPIR-V, HLSL, MSL, and GLSL
    backends. WGSL and SPIR-V backends natively support the instruction.
    MSL and HLSL emulate it by casting to f16 and back to f32. GLSL does
    similar but must (mis)use (un)pack2x16 to do so.

--- naga/src/back/hlsl/writer.rs
+++ naga/src/back/hlsl/writer.rs
@@ -3052,6 +3052,7 @@ impl<'a, W: fmt::Write> super::Writer<'a, W> {
                     Unpack4x8unorm,
                     Unpack4xI8,
                     Unpack4xU8,
+                    QuantizeToF16,
                     Regular(&'static str),
                     MissingIntOverload(&'static str),
                     MissingIntReturnType(&'static str),
@@ -3118,6 +3119,7 @@ impl<'a, W: fmt::Write> super::Writer<'a, W> {
                     //Mf::Inverse =>,
                     Mf::Transpose => Function::Regular("transpose"),
                     Mf::Determinant => Function::Regular("determinant"),
+                    Mf::QuantizeToF16 => Function::QuantizeToF16,
                     // bits
                     Mf::CountTrailingZeros => Function::CountTrailingZeros,
                     Mf::CountLeadingZeros => Function::CountLeadingZeros,
@@ -3318,6 +3320,11 @@ impl<'a, W: fmt::Write> super::Writer<'a, W> {
                         write!(self.out, " >> 16, ")?;
                         self.write_expr(module, arg, func_ctx)?;
                         write!(self.out, " >> 24) << 24 >> 24")?;
+                    }
+                    Function::QuantizeToF16 => {
+                        write!(self.out, "f16tof32(f32tof16(")?;
+                        self.write_expr(module, arg, func_ctx)?;
+                        write!(self.out, "))")?;
                     }
                     Function::Regular(fun_name) => {
                         write!(self.out, "{fun_name}(")?;
