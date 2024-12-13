722b68740350536424bd24e9fd5af7fa40e0eab6
Jim Blandy <jimb@red-bean.com>
2024-12-13 15:49:47 -0800

    [naga valid]: Clean up validation of `Statement::ImageStore`.
    Move image class validation to a more natural spot.

--- naga/src/valid/function.rs
+++ naga/src/valid/function.rs
@@ -1065,6 +1065,14 @@ impl super::Validator {
                         .with_handle(image, context.expressions));
                     };
 
+                    // It had better be a storage image, since we're writing to it.
+                    let crate::ImageClass::Storage { format, .. } = class else {
+                        return Err(FunctionError::InvalidImageStore(
+                            ExpressionError::InvalidImageClass(class),
+                        )
+                        .with_span_handle(image, context.expressions));
+                    };
+
                     // The `coordinate` operand must be a vector of the appropriate size.
                     if !context
                         .resolve_type(coordinate, &self.valid_expression_set)?
@@ -1101,14 +1109,6 @@ impl super::Validator {
                             .with_span_handle(expr, context.expressions));
                         }
                     }
-
-                    // It had better be a storage image, since we're writing to it.
-                    let crate::ImageClass::Storage { format, .. } = class else {
-                        return Err(FunctionError::InvalidImageStore(
-                            ExpressionError::InvalidImageClass(class),
-                        )
-                        .with_span_handle(image, context.expressions));
-                    };
 
                     let value_ty = crate::TypeInner::Vector {
                         size: crate::VectorSize::Quad,
