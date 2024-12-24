dae804edb49fd470c5918710ab061f3e10157c4d
Jim Blandy <jimb@red-bean.com>
2024-12-23 18:45:59 -0800

    [naga] Allow abstract scalars in `modf` and `frexp` results.
    Allow `PredeclaredType::ModfResult` and `PredeclaredType::FrexpResult`
    to hold any sort of scalar, not just a floating-point scalar.
    
    This prepares Naga for implementing the `AbstractFloat` overloads for
    the `modf` and `frexp` builtin functions.

--- naga/src/lib.rs
+++ naga/src/lib.rs
@@ -2221,11 +2221,11 @@ pub enum PredeclaredType {
     AtomicCompareExchangeWeakResult(Scalar),
     ModfResult {
         size: Option<VectorSize>,
-        width: Bytes,
+        scalar: Scalar,
     },
     FrexpResult {
         size: Option<VectorSize>,
-        width: Bytes,
+        scalar: Scalar,
     },
 }
 
