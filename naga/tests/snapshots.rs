89fa6851c80069f17bf0fbd8d09b25844cb13880
Vecvec <130132884+Vecvec@users.noreply.github.com>
2025-01-15 02:28:37 +0000

    Support DXR in wgpu-hal & naga. (#6777)
    Co-authored-by: Connor Fitzgerald <connorwadefitzgerald@gmail.com>

--- naga/tests/snapshots.rs
+++ naga/tests/snapshots.rs
@@ -891,7 +891,7 @@ fn convert_wgsl() {
         ("sprite", Targets::SPIRV),
         ("force_point_size_vertex_shader_webgl", Targets::GLSL),
         ("invariant", Targets::GLSL),
-        ("ray-query", Targets::SPIRV | Targets::METAL),
+        ("ray-query", Targets::SPIRV | Targets::METAL | Targets::HLSL),
         ("hlsl-keyword", Targets::HLSL),
         (
             "constructors",
