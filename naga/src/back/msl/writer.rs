66415c33a34d51e9ea0a0a67e548dbbcf2d5e228
sagudev <16504129+sagudev@users.noreply.github.com>
2025-03-08 18:27:35 +0100

    Revert [wgsl-out] Write const named expr as const declarations
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- naga/src/back/msl/writer.rs
+++ naga/src/back/msl/writer.rs
@@ -17,7 +17,7 @@ use crate::{
     arena::{Handle, HandleSet},
     back::{self, Baked},
     common,
-    proc::{self, index, ExpressionKindTracker, NameKey, TypeResolution},
+    proc::{self, index, NameKey, TypeResolution},
     valid, FastHashMap, FastHashSet,
 };
 
@@ -5575,7 +5575,6 @@ template <typename A>
                 info: &mod_info[fun_handle],
                 expressions: &fun.expressions,
                 named_expressions: &fun.named_expressions,
-                expr_kind_tracker: ExpressionKindTracker::from_arena(&fun.expressions),
             };
 
             writeln!(self.out)?;
@@ -5732,7 +5731,6 @@ template <typename A>
                 info: fun_info,
                 expressions: &fun.expressions,
                 named_expressions: &fun.named_expressions,
-                expr_kind_tracker: ExpressionKindTracker::from_arena(&fun.expressions),
             };
 
             self.write_wrapped_functions(module, &ctx)?;
