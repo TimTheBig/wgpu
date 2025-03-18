7964802adb33916a3d6b1fb13b4b1d428c39f181
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-03-18 14:26:45 -0400

    Cache reserved keywords (#7338)

--- naga/src/back/msl/writer.rs
+++ naga/src/back/msl/writer.rs
@@ -3801,8 +3801,7 @@ impl<W: Write> Writer<W> {
         self.names.clear();
         self.namer.reset(
             module,
-            super::keywords::RESERVED,
-            &[],
+            &super::keywords::RESERVED_SET,
             &[],
             &[CLAMPED_LOD_LOAD_PREFIX],
             &mut self.names,
