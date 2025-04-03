6a5c62cad06a519aa57800b427be17daf520890f
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-04-03 18:39:49 -0400

    [d3d12] Use `DeviceAllocationContext` for deallocation

--- wgpu-hal/src/dx12/device.rs
+++ wgpu-hal/src/dx12/device.rs
@@ -451,10 +451,9 @@ impl crate::Device for super::Device {
 
     unsafe fn destroy_buffer(&self, buffer: super::Buffer) {
         suballocation::free_resource(
-            self,
+            suballocation::DeviceAllocationContext::from(self),
             buffer.resource,
             buffer.allocation,
-            &self.mem_allocator,
         );
 
         self.counters.buffers.sub(1);
@@ -542,11 +541,9 @@ impl crate::Device for super::Device {
 
     unsafe fn destroy_texture(&self, texture: super::Texture) {
         suballocation::free_resource(
-            self,
+            suballocation::DeviceAllocationContext::from(self),
             texture.resource,
             texture.allocation,
-            // SAFETY: for allocations to exist, the allocator must exist
-            &self.mem_allocator,
         );
 
         self.counters.textures.sub(1);
@@ -1678,10 +1675,9 @@ impl crate::Device for super::Device {
 
         if let Some(sampler_buffer) = group.sampler_index_buffer {
             suballocation::free_resource(
-                self,
+                suballocation::DeviceAllocationContext::from(self),
                 sampler_buffer.buffer,
                 sampler_buffer.allocation,
-                &self.mem_allocator,
             );
         }
 
@@ -2325,10 +2321,9 @@ impl crate::Device for super::Device {
         acceleration_structure: super::AccelerationStructure,
     ) {
         suballocation::free_resource(
-            self,
+            suballocation::DeviceAllocationContext::from(self),
             acceleration_structure.resource,
             acceleration_structure.allocation,
-            &self.mem_allocator,
         );
     }
 
