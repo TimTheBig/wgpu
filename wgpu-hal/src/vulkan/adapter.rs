6ad8413013311929ee94b9866891931c06f903f2
teoxoy <28601907+teoxoy@users.noreply.github.com>
2025-04-23 16:13:04 +0200

    [vk] move framebuffer cache into `CommandEncoder`
    This makes texture view destruction cheap since users of wgpu-hal are required to keep resources referenced by encoders alive for at least as long as the encoder is alive.
    
    This is also a prerequisite to implement rendering to slices of 3D textures (by creating temporary texture views).
    
    On the other hand the cache won't be as effective but that is probably ok, we can reevaluate the implementation if it turns out to be a problem.

--- wgpu-hal/src/vulkan/adapter.rs
+++ wgpu-hal/src/vulkan/adapter.rs
@@ -2068,7 +2068,6 @@ impl super::Adapter {
             features,
             workarounds: self.workarounds,
             render_passes: Mutex::new(Default::default()),
-            framebuffers: Mutex::new(Default::default()),
             sampler_cache: Mutex::new(super::sampler::SamplerCache::new(
                 self.private_caps.maximum_samplers,
             )),
