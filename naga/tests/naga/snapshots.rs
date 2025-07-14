070a5a97a08f8ff67e599684f3977327878d2fe2
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2025-07-14 00:27:13 -0400

    Label no-op supporting tests

--- naga/tests/naga/snapshots.rs
+++ naga/tests/naga/snapshots.rs
@@ -837,7 +837,8 @@ fn write_output_wgsl(
 
 // While we _can_ run this test under miri, it is extremely slow (>5 minutes),
 // and naga isn't the primary target for miri testing, so we disable it.
-#[cfg(all(feature = "wgsl-in", not(miri)))]
+#[cfg(feature = "wgsl-in")]
+#[cfg_attr(miri, ignore)]
 #[test]
 fn convert_snapshots_wgsl() {
     let _ = env_logger::try_init();
@@ -863,7 +864,8 @@ fn convert_snapshots_wgsl() {
 }
 
 // miri doesn't allow us to shell out to `spirv-as`
-#[cfg(all(feature = "spv-in", not(miri)))]
+#[cfg(feature = "spv-in")]
+#[cfg_attr(miri, ignore)]
 #[test]
 fn convert_snapshots_spv() {
     use std::process::Command;
@@ -914,7 +916,8 @@ fn convert_snapshots_spv() {
 
 // While we _can_ run this test under miri, it is extremely slow (>5 minutes),
 // and naga isn't the primary target for miri testing, so we disable it.
-#[cfg(all(feature = "glsl-in", not(miri)))]
+#[cfg(feature = "glsl-in")]
+#[cfg_attr(miri, ignore)]
 #[allow(unused_variables)]
 #[test]
 fn convert_snapshots_glsl() {
